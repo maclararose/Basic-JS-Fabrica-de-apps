20edb92174d27403a6fb761feef2c874b7130ffa
maclararose <maria.uespi@outlook.com>
2020-03-22 00:49:47 -0300

    Learning loops

---
+++ project 5 - aula 13/script.js
@@ -0,0 +1,16 @@
+x = 0;
+
+while(x < 10){
+    
+    document.write("<br/>O valor do x e: "+x);
+
+    x++;
+
+}
+
+document.write("<br/><br/><br/>");
+
+for(a = 0; a < 10; a++){
+    document.write("<br/>O valor de a : "+ a);
+
+}
